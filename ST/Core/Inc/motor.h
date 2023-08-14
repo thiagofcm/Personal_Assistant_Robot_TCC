30d6c6bd4c53e4fe6bd94bd0322fa62bd3f61aef
thiagofcm <thiagof1503@gmail.com>
2023-08-14 15:44:53 -0300

    ST-driver-motor

---
+++ ST/Core/Inc/motor.h
@@ -0,0 +1,14 @@
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+void motor_frente(void);
+
+void motor_right(void);
+
+void motor_left(void);
+
+void motor_re(void);
+
+void motor_parado(void);
+
+#endif /* MOTOR_H_ */
